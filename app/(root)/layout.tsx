37f289ecd5520d81f67111b09fc058dc22bd82bc
itzfahadoooo <foyindolapo@gmail.com>
2024-11-25 18:55:18 +0100

    Initial commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
-import React from "react"
-import Navbar from "../components/Navbar"
+import React from "react";
+import Navbar from "../../components/ui/Navbar";
 
-export default function layout({children}: Readonly<{children: React.ReactNode}>){
-    return (
-        <main className="font-work-sans">
-            <Navbar/>
-            
-            {children}
-        </main>
-    )
+export default function layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+
+      {children}
+    </main>
+  );
 }
--- components/ui/SearchForm.tsx
+++ components/ui/SearchForm.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Form from "next/form";
 import SearchFormReset from "./SearchFormReset";
+import { Search } from "lucide-react";
 
-const SearchForm = ({query}:{query?: string}) => {
-    
+const SearchForm = ({ query }: { query?: string }) => {
   return (
     <Form action="/" scroll={false} className="search-form">
       <input
@@ -13,12 +13,12 @@ const SearchForm = ({query}:{query?: string}) => {
         placeholder="Search Startups"
       />
 
-        <div className="flex gap-2">
-              {query && <SearchFormReset />}
-              
-              <button type="submit" className="search-btn text-white">
-                  S
-                </button>
+      <div className="flex gap-2">
+        {query && <SearchFormReset />}
+
+        <button type="submit" className="search-btn text-white">
+          <Search className="size-5"/>
+        </button>
       </div>
     </Form>
   );
